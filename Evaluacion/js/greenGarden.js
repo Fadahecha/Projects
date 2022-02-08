b7562038661410747f3ed195cf539576a4b8eb70
Fadahecha <fadahecha@gmail.com>
2022-02-08 17:00:02 +0100

    bananas

---
+++ Evaluacion/js/greenGarden.js
@@ -0,0 +1,19 @@
+/*nav fixed avec le scroll*/
+
+posicionarMenu();
+
+$(window).scroll(function () {
+  posicionarMenu();
+});
+
+function posicionarMenu() {
+  var altura_del_header = $(".barra-logo").outerHeight(true);
+  
+  if ($(window).scrollTop() >= altura_del_header) {
+    $(".barra").addClass("fixed"); 
+  } else {
+    $(".barra").removeClass("fixed");       
+  }
+}
+
+/*****************************************************************************/
